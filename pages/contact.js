8c0b95ca073c3ff48fcacea215ab0d246e3f1d3e
Máximo Aráuz <elmogof@outlook.com>
2022-03-24 15:45:17 -0600

    minor fixes

--- pages/contact.js
+++ pages/contact.js
@@ -2,10 +2,17 @@ import React from 'react'
 import GenericTitleComponent from '../components/GenericTitleComponent'
 
 export const Contact = () => {
+    const persona = {
+        id: 12345678,
+        nombre: 'Alberto',
+        apellido: 'Cardenas'
+      };
     return (
         <div>
             <GenericTitleComponent
-                texto = 'Contact Page'
+                texto='Contact Page'
+                persona={persona}
+                numero={1}
             />
         </div>
     )
